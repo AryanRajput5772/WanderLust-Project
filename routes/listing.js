93f4430cadc84b98c36c5a3da372c1b9d9af61fc
AryanRajput5772 <144768314+AryanRajput5772@users.noreply.github.com>
2024-04-16 19:08:42 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -20,61 +20,59 @@ router.route("/").get(wrapAsync(listingController.index)).post(
 
 router.post(
   "/search",
-  wrapAsync(listingController.searchRoute);
-
-    
-  
+  wrapAsync(listingController.searchRoute)
 );
 ///////////////filter route/////////////////////////////////////////////////////////////////
 //1 --> /listings/Boats
 router.get(
   "/Boats",
-  wrapAsync(listingController.Boats);
-
+  wrapAsync(listingController.Boats)
+);
 //2 --> /listings/Domes
 router.get(
   "/Domes",
-  wrapAsync(listingController.Domes);
-
+  wrapAsync(listingController.Domes)
+);
 //3 --> /listings/Arctic
 router.get(
   "/Arctic",
-  wrapAsync(listingController.Arctic);
-
+  wrapAsync(listingController.Arctic)
+);
 //4 --> /listings/Farms
 router.get(
   "/Farms",
-  wrapAsync(listingController.Farms);
-
+  wrapAsync(listingController.Farms)
+);
 //5 --> /listings/Camping
 router.get(
   "/Camping",
-  wrapAsync(listingController.Camping);
-
+  wrapAsync(listingController.Camping)
+);
 //6 --> /listings/AmazingPools
 router.get(
   "/AmazingPools",
-  wrapAsync(listingController.AmazingPools);
-
+  wrapAsync(listingController.AmazingPools)
+);
 //7 --> /listings/Beach
 router.get(
   "/Beach",
-  wrapAsync(listingController.Beach);
-
+  wrapAsync(listingController.Beach)
+);
 //8 --> /listings/Mountains
 router.get(
   "/Mountains",
-  wrapAsync(listingController.Mountains);
-
+  wrapAsync(listingController.Mountains)
+);
 //9 --> /listings/IconicCities
 router.get(
   "/IconicCities",
-  wrapAsync(listingController.IconicCities);
-
+  wrapAsync(listingController.IconicCities)
+);
 //10 --> /listings/Villas
 router.get(
   "/Villas",
-  wrapAsync(listingController.Villas);
+  wrapAsync(listingController.Villas)
+);
 ///////////////New Route////////////////////
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
