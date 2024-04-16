788a6b724847aed75f45911b85487ed776f62181
AryanRajput5772 <144768314+AryanRajput5772@users.noreply.github.com>
2024-04-16 18:53:59 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -20,134 +20,70 @@ router.route("/").get(wrapAsync(listingController.index)).post(
 
 router.post(
   "/search",
-  wrapAsync(async (req, res) => {
-    let search = req.body.search;
-    let searchText = search.toLowerCase();
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => {
-      if (el.country.toLowerCase().includes(searchText)) {
-        return el;
-      }
-      if (el.title.toLowerCase().includes(searchText)) {
-        return el;
-      }
-      if (el.location.toLowerCase().includes(searchText)) {
-        return el;
-      } else if (typeof parseInt(searchText) == "number") {
-        if (el.price > parseInt(searchText)) {
-          return el;
-        }
-      }
-    });
+  wrapAsync(listingController.searchRoute );
 
-    res.render("listings/search.ejs", { matchingListing, allListings });
-  })
+    
+  
 );
 ///////////////filter route/////////////////////////////////////////////////////////////////
 //1 --> /listings/Boats
 router.get(
   "/Boats",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Boats");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Boats
 );
 
 //2 --> /listings/Domes
 router.get(
   "/Domes",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Domes");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Domes
 );
 
 //3 --> /listings/Arctic
 router.get(
   "/Arctic",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Arctic");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Arctic
 );
 
 //4 --> /listings/Farms
 router.get(
   "/Farms",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Farms");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Farms
 );
 
 //5 --> /listings/Camping
 router.get(
   "/Camping",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Camping");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Camping
 );
 
 //6 --> /listings/AmazingPools
 router.get(
   "/AmazingPools",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter(
-      (el) => el.category === "Amazing Pools"
-    );
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.AmazingPools
 );
 
 //7 --> /listings/Beach
 router.get(
   "/Beach",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Beach");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Beach
 );
 
 //8 --> /listings/Mountains
 router.get(
   "/Mountains",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter(
-      (el) => el.category === "Mountains"
-    );
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Mountains
 );
 
 //9 --> /listings/IconicCities
 router.get(
   "/IconicCities",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter(
-      (el) => el.category === "Iconic cities"
-    );
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.IconicCities
 );
 
 //10 --> /listings/Villas
 router.get(
   "/Villas",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    let matchingListing = allListings.filter((el) => el.category === "Villas");
-    res.render("listings/index.ejs", { matchingListing });
-  })
+  wrapAsync(listingController.Villas
 );
 ///////////////New Route////////////////////
 router.get("/new", isLoggedIn, listingController.renderNewForm);
